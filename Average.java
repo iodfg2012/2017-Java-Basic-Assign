b877bcd5daeb88aa5f507c2d3a036ad02d759f04
iodfg2012 <iodfg2012@daum.net>
2017-03-21 15:27:55 +0900

    Update Average.java
    20141644 김종준 과제수행

--- Average.java
+++ Average.java
@@ -10,6 +10,17 @@ public class Average { 
         System.out.print("실수 " + size + "개를 입력하세요. >> ");    
         
         //프로그래밍 하세요.
+        int i = 0, n = 0;
+		double sum = 0;
+		
+		double[] arry = new double[size];
+		
+		for(;i<size;i++){	
+			arry[i] = input.nextDouble();
+			sum += arry[i];
+			if(arry[i]==0) break;
+			else n++;
+		}
         
         System.out.println("합: " + sum + ", 평균: " + sum/size);
     }
